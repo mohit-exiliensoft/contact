679525659b385f4769c8c087159c1204a6cc108c
Exilien-05 <mohit@exiliensoft.com>
2024-11-14 12:18:20 +0530

    fix

--- src/ContactServiceProvider.php
+++ src/ContactServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace exiliensoft\Contact;
+namespace Exiliensoft\Contact;
 
 use Illuminate\Support\ServiceProvider;
 
@@ -13,6 +13,12 @@ class ContactServiceProvider extends ServiceProvider
         $this->loadRoutesFrom(__DIR__.'/routes/web.php');
         $this->loadViewsFrom(__DIR__.'/views', 'contact');
         $this->loadMigrationsFrom(__DIR__.'/database/migrations');
+        $this->mergeConfigFrom(
+            __DIR__.'/config/contact.php', 'contact'
+        );
+        $this->publishes([
+            __DIR__.'/config/contact.php' => config_path('contact.php'),
+        ]);
 
     }
 
---
+++ src/config/contact.php
@@ -0,0 +1,5 @@
+<?php 
+
+return [
+    'send_email_to' => 'mohitkumar44103@gmail.com'
+];
